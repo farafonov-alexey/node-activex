a881abee6800a11281cfbed14703e4c28b2958ff
Alexey Farafonov <farafonov.alexey@gmail.com>
2018-02-06 14:58:06 +0300

    - remove require for other OS'es

--- activex.js
+++ activex.js
@@ -1,9 +1,15 @@
-var binary = require('node-pre-gyp');
-var path = require('path');
-var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
-var binding = require(binding_path);
-var ActiveX = module.exports = exports = binding;
+var ActiveX = null;
+if (process.platform === "win32" && process.arch === "ia32") {
+    var binary = require('node-pre-gyp');
+    var path = require('path');
+    var binding_path = binary.find(path.resolve(path.join(__dirname, './package.json')));
+    var binding = require(binding_path);
+    ActiveX = module.exports = exports = binding;
+    global.ActiveXObject = function (id, opt) {
+        return new ActiveX.Object(id, opt);
+    };
+} else {
+    ActiveX = module.exports = exports = {};
+}
 
-global.ActiveXObject = function(id, opt) {
-    return new ActiveX.Object(id, opt);
-};
+
